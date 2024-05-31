f4290d86e4bfb0452bba921eee27ee24bfbf1ffd
shallon <shallonmokeira@gmail.com>
2024-05-31 23:03:00 +0300

    add components in app.js to display in web browser

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import TopCardMetrics from './components/Dashboard/TopCardMetric';
+//import Dashboard from './components/Dashboard/Dashboard';
+
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="main-content">
+        <TopCardMetrics />
+        
+      </div>
     </div>
+  </Router>
   );
 }
 
