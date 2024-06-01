85887b14adc4b1bbdb9842c34086230e5dd34d52
shallon <shallonmokeira@gmail.com>
2024-06-01 22:43:54 +0300

    feat:incorporate components to main content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import TopCardMetrics from './components/Dashboard/TopCardMetric';
+import UpcomingInvoices from './components/Dashboard/UpcomingInvoices';
 //import Dashboard from './components/Dashboard/Dashboard';
+import Schools from './components/Schools/Schools';
 
 import './App.css';
 
@@ -13,7 +15,8 @@ function App() {
       <Navbar />
       <div className="main-content">
         <TopCardMetrics />
-        
+        <UpcomingInvoices />
+        <Schools />
       </div>
     </div>
   </Router>
