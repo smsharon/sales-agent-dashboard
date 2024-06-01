f3baa9d521db0e70a0684631442b02b759490366
shallon <shallonmokeira@gmail.com>
2024-06-01 23:27:04 +0300

    incorporate dashboard to the main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import TopCardMetrics from './components/Dashboard/TopCardMetric';
-import UpcomingInvoices from './components/Dashboard/UpcomingInvoices';
-//import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import Schools from './components/Schools/Schools';
 
 import './App.css';
@@ -14,8 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="main-content">
-        <TopCardMetrics />
-        <UpcomingInvoices />
+        <Dashboard />
         <Schools />
       </div>
     </div>
